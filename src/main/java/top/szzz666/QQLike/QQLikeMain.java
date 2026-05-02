f38d2721e49f8e2bf69452ca762d17cfd593706b
szzz666 <3214948198@qq.com>
2026-05-02 22:37:24 +0800

    feat: 集成QQEssential并重构点赞处理逻辑

--- src/main/java/top/szzz666/QQLike/QQLikeMain.java
+++ src/main/java/top/szzz666/QQLike/QQLikeMain.java
@@ -5,17 +5,20 @@ import cn.nukkit.command.CommandSender;
 import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.plugin.PluginLogger;
+import top.szzz666.QQEssential.command.QQCommandManage;
 import top.szzz666.QQLike.command.MyCommand;
 import top.szzz666.QQLike.config.EasyConfig;
 import top.szzz666.QQLike.config.LikeData;
 import top.szzz666.QQLike.listener.QQListeners;
-import top.szzz666.StarrySkyLink.qq.listener.QQEventSource;
+import top.szzz666.StarrySkyLink.qq.entity.Event;
+import top.szzz666.StarrySkyLink.qq.listener.QQEventManage;
 import top.szzz666.StarrySkyLink.websocket.Wss;
 
 import static top.szzz666.CronTaskApi.tools.TaskUtil.Cron;
+import static top.szzz666.QQEssential.command.QQCommand.*;
 import static top.szzz666.QQLike.config.MyConfig.*;
-import static top.szzz666.StarrySkyLink.qq.send.Send.LikeMsg;
-import static top.szzz666.StarrySkyLink.qq.send.Send.PrivateTextMsg;
+import static top.szzz666.StarrySkyLink.qq.send.Send.*;
+import static top.szzz666.StarrySkyLink.qq.send.Send.GroupAtReplyMsg;
 
 
 public class QQLikeMain extends PluginBase {
@@ -43,12 +46,42 @@ public class QQLikeMain extends PluginBase {
     @Override
     public void onEnable() {
         //注册监听器
-        QQEventSource.registerListener(new QQListeners());
+//        QQEventManage.registerListener(new QQListeners());
         //注册命令
         nkServer.getCommandMap().register(this.getName(), new MyCommand());
+
+        QQCommandManage.register(trigger_key, "点赞你主页点赞", "点赞",
+                PERM_ALL, SCOPE_BOTH, QQLikeMain::onLike);
         startAutoLike();
         logger.info("§b" + plugin.getName() + "插件开启");
         logger.warning("§c" + plugin.getName() + "如果遇到任何bug，请加入Q群进行反馈：894279534");
+    }
+
+    private static void onLike(Event event, String s, String[] strings) {
+        long user_id = event.getUser_id();
+        // 检查每日点赞次数限制
+        int usedCount = LikeData.getLikeCount(user_id);
+        if (usedCount >= like_times_per_day) {
+            String limitMsg = like_limit_msg
+                    .replace("%used%", String.valueOf(usedCount))
+                    .replace("%max%", String.valueOf(like_times_per_day));
+            if (event.isMineGroupMessage()) {
+                event.send(GroupAtReplyMsg(event.getGroup_id(), user_id, event.getMessage_id(), "\n" + limitMsg));
+            }
+            if (event.isPrivateMessage()) {
+                event.send(PrivateTextMsg(user_id, limitMsg));
+            }
+            return;
+        }
+
+        if (event.isMineGroupMessage()) {
+            event.send(GroupAtReplyMsg(event.getGroup_id(), user_id, event.getMessage_id(), "\n" + reply_msg));
+        }
+        if (event.isPrivateMessage()) {
+            event.send(PrivateTextMsg(user_id, reply_msg));
+        }
+        event.send(LikeMsg(user_id, like_times));
+        LikeData.incrementLikeCount(user_id);
     }
 
     //插件关闭
